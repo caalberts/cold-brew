48ba0d4ac96907a57bebb7aecf3da57b24927e83
Albert Salim <albertlimca@gmail.com>
2016-11-15 12:30:07 +0800

    Add test baseline

---
+++ test/coldbrew.test.js
@@ -0,0 +1,10 @@
+import ColdBrew from '../src/coldbrew'
+import { expect } from 'chai'
+
+describe('ColdBrew#caffeinate', () => {
+  let coldBrew
+
+  before('set up test', () => {
+    coldBrew = new ColdBrew()
+  })
+})
