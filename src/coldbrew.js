8addaace866195bbfa7d758c795bf71f1639defe
Albert Salim <albertlimca@gmail.com>
2016-11-15 20:30:16 +0800

    refactor caffeinate

--- src/coldbrew.js
+++ src/coldbrew.js
@@ -1,24 +1,19 @@
 export default class ColdBrew {
   caffeinate(text) {
     let letters = text.split('')
-    let result = ''
 
     let vowelRatio = this.calculateVowelRatio(letters)
-    if (vowelRatio > 0.3) {
-      let caffeinated = letters.map((letter, index) => {
-        if (this.isVowel(letter)) {
-          let previousLetter = letters[index - 1]
-          return this.isVowel(previousLetter) ? '' : 'coffee'
-        } else {
-          return letter
-        }
-      })
-      result = caffeinated.join('')
-    } else {
-      result = text
-    }
+    if (vowelRatio <= 0.3) return text
 
-    return result
+    let caffeinated = letters.map((letter, index) => {
+      if (this.isVowel(letter)) {
+        let previousLetter = letters[index - 1]
+        return this.isVowel(previousLetter) ? '' : 'coffee'
+      } else {
+        return letter
+      }
+    })
+    return caffeinated.join('')
   }
 
   isVowel(letter) {
